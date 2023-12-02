1772937ef18391daadc585ee214bcccfee97f662
Mike Dalton <michaelcdalton@gmail.com>
2023-12-01 22:26:46 -0500

    day 1, part 2

--- one.rs
+++ one.rs
@@ -12,9 +12,33 @@ fn main() {
     let mut count: i32 = 0;
     for line in read_lines("./one.txt") {
         let mut numbers = Vec::<char>::new();
+        let mut word = String::from("");
         for c in line.chars() {
             if c.is_numeric() {
                 numbers.push(c);
+                word = String::from("");
+            } else {
+                word.push(c);
+                
+                if word.ends_with("one") {
+                    numbers.push('1');
+                } else if word.ends_with("two") {
+                    numbers.push('2');
+                } else if word.ends_with("three") {
+                    numbers.push('3');
+                } else if word.ends_with("four") {
+                    numbers.push('4');
+                } else if word.ends_with("five") {
+                    numbers.push('5');
+                } else if word.ends_with("six") {
+                    numbers.push('6');
+                } else if word.ends_with("seven") {
+                    numbers.push('7');
+                } else if word.ends_with("eight") {
+                    numbers.push('8');
+                } else if word.ends_with("nine") {
+                    numbers.push('9');
+                }
             }
         }
         
